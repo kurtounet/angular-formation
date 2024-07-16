ba7c407493253cfbd0a8fc0fa13aa88989b2f9cc
Anthony bossut <bossut.anthony@gmail.com>
2024-07-16 13:00:15 +0200

    add routes, update appcomponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CourseListComponentComponent } from './course-list-component/course-list-component.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '',component: HomeComponent }, 
+    { path: 'courses',component: CourseListComponentComponent },
+
+];
